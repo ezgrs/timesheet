7394afda4b34ad436ba05802412c2f2d2ab55900
Enzo Santos <enzo.santos29@hotmail.com>
2026-06-01 03:40:18 -0300

    [users] feat: use TypeOrmModule.forFeature for EmployeeModule

--- users-service/src/infrastructure/api/modules/employee.module.ts
+++ users-service/src/infrastructure/api/modules/employee.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common"
 import { EmployeeController } from "../controllers/employee.controller.js"
 import { TypeORMEmployeeRepository } from "../services/typeorm-employee.repository.js"
+import { EmployeeEntity } from "@/infrastructure/db/entities/employee.entity.js"
+import { TypeOrmModule } from "@nestjs/typeorm"
 
 @Module({
+    imports: [TypeOrmModule.forFeature([EmployeeEntity])],
     controllers: [EmployeeController],
     providers: [TypeORMEmployeeRepository],
 })
--- users-service/src/infrastructure/api/services/typeorm-employee.repository.ts
+++ users-service/src/infrastructure/api/services/typeorm-employee.repository.ts
@@ -3,18 +3,22 @@ import { EmployeeRepository } from "@/application/ports/employee.repository.js"
 import { Employee } from "@/domain/entities/employee.js"
 import { EmployeeMapper } from "@/infrastructure/db/mappers/employee.mapper.js"
 import { EmployeeEntity } from "@/infrastructure/db/entities/employee.entity.js"
-import { DataSource } from "typeorm"
+import { Repository } from "typeorm"
+import { InjectRepository } from "@nestjs/typeorm"
 
 @Injectable()
 export class TypeORMEmployeeRepository implements EmployeeRepository {
-    constructor(private readonly dataSource: DataSource) {}
+    constructor(
+        @InjectRepository(EmployeeEntity)
+        private readonly repository: Repository<EmployeeEntity>,
+    ) {}
 
     async create(data: Employee): Promise<void> {
         const entity = EmployeeMapper.toEntity(data)
-        await entity.save()
+        await this.repository.save(entity)
     }
 
     async delete(id: string): Promise<void> {
-        await this.dataSource.manager.delete(EmployeeEntity, { id })
+        await this.repository.delete({ id })
     }
 }
