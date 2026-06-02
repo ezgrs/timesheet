4c30258f6bca801bba79ca6ff71d5fc574d65623
Enzo Santos <enzo.santos29@hotmail.com>
2026-06-02 00:25:16 -0300

    [web] style: run prettier

--- web-service/src/app/app.ts
+++ web-service/src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
+import { HomeComponent } from "../home/component"
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: "app-root",
+    imports: [RouterOutlet, HomeComponent],
+    templateUrl: "./app.html",
+    styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal('timesheet');
+    protected readonly title = signal("timesheet")
 }
