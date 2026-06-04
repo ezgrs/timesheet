318589831c433ed36d9a106f030c37778672c0f9
Enzo Santos <enzo.santos29@hotmail.com>
2026-06-03 22:54:56 -0300

    [users] feat: add parameters to global ValidationPipe

--- users-service/src/main.ts
+++ users-service/src/main.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./infrastructure/api/modules/app.module.js"
 import { ValidationPipe } from "@nestjs/common"
@@ -6,8 +7,9 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.useGlobalPipes(
         new ValidationPipe({
+            whitelist: true,
             transform: true,
-            forbidUnknownValues: true,
+            forbidNonWhitelisted: true,
         }),
     )
     await app.listen(process.env.PORT ?? 3000)
