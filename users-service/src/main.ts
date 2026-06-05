50da5c3352964e4fb03f85af12f7a062f1fe95cd
Enzo Santos <enzo.santos29@hotmail.com>
2026-06-05 19:38:55 -0300

    [users] feat: add CORS based on CORS_ORIGIN env variable

--- users-service/src/main.ts
+++ users-service/src/main.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata"
+import "dotenv/config"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./infrastructure/api/modules/app.module.js"
 import { ValidationPipe } from "@nestjs/common"
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    app.enableCors({origin: process.env.CORS_ORIGIN})
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
